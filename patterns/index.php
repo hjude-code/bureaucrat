45f47f55cd4be287e4f078ff521925405e608895
Harrison Jude <harrison@hjude.com>
2024-05-08 13:11:16 -0400

    removed bullet from list style

--- patterns/index.php
+++ patterns/index.php
@@ -6,51 +6,33 @@
  * Inserter: no
  */
 ?>
-<!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"backgroundColor":"contrast","textColor":"base-2"} -->
-<h2 class="wp-block-heading has-base-2-color has-contrast-background-color has-text-color has-background has-link-color">Hey there you, let's create something remarkable together!</h2>
+<!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"backgroundColor":"contrast","textColor":"base-2","className":"is-style-offset","fontSize":"x-large"} -->
+<h2 class="wp-block-heading is-style-offset has-base-2-color has-contrast-background-color has-text-color has-background has-link-color has-x-large-font-size" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">Hey there you, let's create something remarkable together!</h2>
 <!-- /wp:heading -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"},"position":{"type":"sticky","top":"0px"},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"backgroundColor":"contrast","textColor":"base-2","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group has-base-2-color has-contrast-background-color has-text-color has-background has-link-color"><!-- wp:site-logo {"width":25,"shouldSyncIcon":true,"style":{"color":{"duotone":"var:preset|duotone|grayscale"}}} /-->
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"backgroundColor":"contrast","textColor":"base-2","className":"is-style-primary-navigation","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group is-style-primary-navigation has-base-2-color has-contrast-background-color has-text-color has-background has-link-color"><!-- wp:site-logo {"width":43,"className":"is-style-invert"} /-->
 
-<!-- wp:heading -->
-<h2 class="wp-block-heading">Harrison Jude</h2>
-<!-- /wp:heading --></div>
+<!-- wp:site-title {"fontSize":"medium"} /-->
+
+<!-- wp:spacer {"style":{"layout":{"flexSize":null,"selfStretch":"fill"}}} -->
+<div aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:navigation {"ref":6,"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} /--></div>
 <!-- /wp:group -->
 
-<!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading -->
-<h2 class="wp-block-heading">Graphic Design & Creative Development</h2>
-<!-- /wp:heading --></div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:social-links -->
-<ul class="wp-block-social-links"><!-- wp:social-link {"url":"codepen.com","service":"codepen"} /-->
-
-<!-- wp:social-link {"url":"dribbble.com","service":"dribbble"} /-->
-
-<!-- wp:social-link {"url":"github.com","service":"github"} /-->
-
-<!-- wp:social-link {"url":"instagram.com","service":"instagram"} /-->
-
-<!-- wp:social-link {"url":"linkedin.com","service":"linkedin"} /--></ul>
-<!-- /wp:social-links --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
+<!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"backgroundColor":"contrast","textColor":"base-2","fontSize":"medium"} -->
+<h2 class="wp-block-heading has-base-2-color has-contrast-background-color has-text-color has-background has-link-color has-medium-font-size">Graphic Design & Creative Development</h2>
+<!-- /wp:heading -->
 
 <!-- wp:query {"queryId":31,"query":{"perPage":"6","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"align":"full","layout":{"type":"default"}} -->
 <div class="wp-block-query alignfull"><!-- wp:post-template {"style":{"typography":{"textTransform":"none"}},"layout":{"type":"grid","columnCount":3}} -->
-<!-- wp:post-title /-->
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"contrast","isUserOverlayColor":true,"minHeight":50,"minHeightUnit":"vh","contentPosition":"bottom center","style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}},"heading":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2","className":"is-style-link-cover","layout":{"type":"default"}} -->
+<div class="wp-block-cover has-custom-content-position is-position-bottom-center is-style-link-cover has-base-2-color has-text-color has-link-color" style="min-height:50vh"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"is-style-preview-pane","layout":{"type":"default"}} -->
+<div class="wp-block-group is-style-preview-pane"><!-- wp:post-title {"level":1,"isLink":true,"linkTarget":"_blank","style":{"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}}} /-->
 
-<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"borderDark","isUserOverlayColor":true,"className":"is-style-link-cover","layout":{"type":"constrained"}} -->
-<div class="wp-block-cover is-style-link-cover"><span aria-hidden="true" class="wp-block-cover__background has-border-dark-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"className":"is-style-preview-pane","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-preview-pane"><!-- wp:post-title {"isLink":true} /-->
-
-<!-- wp:paragraph -->
-<p>test of a preivew pane</p>
-<!-- /wp:paragraph --></div>
+<!-- wp:post-terms {"term":"post_tag"} /--></div>
 <!-- /wp:group --></div></div>
 <!-- /wp:cover -->
 <!-- /wp:post-template --></div>
@@ -66,18 +48,18 @@
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"50vw","className":"is-style-sticky-column-desktop"} -->
-<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:50vw"><!-- wp:heading -->
-<h2 class="wp-block-heading">Heading</h2>
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:50vw"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Oh Hi there, I'm Harrison Jude.<br>My Head is in the clouds,<br>but my feet are on the ground.</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p>A graphic Designer and Creative Developer<br>I’m not afraid to say “I don’t know.” In fact, I embrace it. It’s the gateway to the unknown, the spark of curiosity that ignites insight, fuels creation, and uncovers solutions.<br>As a creative developer, I don’t just solve problems, I craft visual solutions. I build systems that are as beautiful as they are effective.</p>
+<p>I’m not afraid to say “I don’t know.” In fact, I embrace it. It’s the gateway to the unknown, the spark of curiosity that ignites insight, fuels creation, and uncovers solutions.<br>As a creative developer, I don’t just solve problems, I craft visual solutions. I build systems that are as beautiful as they are effective.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:image {"id":428,"sizeSlug":"large","linkDestination":"none"} -->
-<figure class="wp-block-image size-large"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/headshot_me_peak-1024x606.webp" alt="" class="wp-image-428"/></figure>
+<!-- wp:column {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+<div class="wp-block-column" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:image {"id":428,"sizeSlug":"large","linkDestination":"none","className":"is-style-peakaboo"} -->
+<figure class="wp-block-image size-large is-style-peakaboo"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/images/headshot_me_peak-1024x606.webp" alt="" class="wp-image-428"/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
@@ -94,18 +76,18 @@
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"50vw","className":"is-style-sticky-column-desktop"} -->
-<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:50vw"><!-- wp:heading -->
-<h2 class="wp-block-heading">Design that does more than just look good, Design that works.</h2>
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:50vw"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Design that does more than just look good,<br><mark style="background-color:rgba(0, 0, 0, 0);color:#cf2e2e" class="has-inline-color">Design that works.</mark></h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>If the foundations aren’t stable, everything built on them will suffer. I build robust and meaningful design systems that help ensure success across all aspects of the project.</p>
+<!-- wp:paragraph {"fontSize":"medium"} -->
+<p class="has-medium-font-size">I build design systems that empower you to create impactful graphics. From the start, I focus on strong foundations to ensure a stable and successful project. I then create user-friendly toolkits with robust asset libraries, template files, and even advanced features like After Effects expressions. This ensures visual consistency and makes working within the system painless. Finally, I construct these systems with scalability in mind, so they can grow and evolve alongside your campaigns and brands. The result? Impactful graphics that reliably grab attention and deliver your message.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:list {"style":{"spacing":{"padding":{"right":"0","left":"0"}}},"fontSize":"x-large"} -->
-<ul style="padding-right:0;padding-left:0" class="has-x-large-font-size"><!-- wp:list-item -->
+<div class="wp-block-column"><!-- wp:list {"style":{"spacing":{"padding":{"right":"0","left":"0","top":"0","bottom":"0"}}},"className":"is-style-cell-list","fontSize":"x-large"} -->
+<ul style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0" class="is-style-cell-list has-x-large-font-size"><!-- wp:list-item -->
 <li>Motion Design</li>
 <!-- /wp:list-item -->
 
@@ -125,8 +107,8 @@
 <li>Front End Development</li>
 <!-- /wp:list-item -->
 
-<!-- wp:list-item {"style":{"spacing":{"padding":{"right":"0","left":"0"}}}} -->
-<li style="padding-right:0;padding-left:0">Project Management</li>
+<!-- wp:list-item -->
+<li>Project Management</li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></div>
 <!-- /wp:column --></div>
@@ -144,8 +126,8 @@
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"50vw","className":"is-style-sticky-column-desktop"} -->
-<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:50vw"><!-- wp:heading -->
-<h2 class="wp-block-heading">Heading</h2>
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:50vw"><!-- wp:heading {"fontSize":"large"} -->
+<h2 class="wp-block-heading has-large-font-size">Software, Code, and a little je ne sais quoi</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
@@ -154,8 +136,8 @@
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:list {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
-<ul style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:list-item -->
+<div class="wp-block-column"><!-- wp:list {"style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}}},"className":"is-style-cell-list"} -->
+<ul style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0" class="is-style-cell-list"><!-- wp:list-item -->
 <li>Illustrator</li>
 <!-- /wp:list-item -->
 
@@ -194,8 +176,8 @@
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:list {"style":{"spacing":{"padding":{"right":"0","left":"0"}}}} -->
-<ul style="padding-right:0;padding-left:0"><!-- wp:list-item -->
+<div class="wp-block-column"><!-- wp:list {"style":{"spacing":{"padding":{"right":"0","left":"0"}}},"className":"is-style-cell-list"} -->
+<ul style="padding-right:0;padding-left:0" class="is-style-cell-list"><!-- wp:list-item -->
 <li>HTML/CSS/JS</li>
 <!-- /wp:list-item -->
 
@@ -230,9 +212,13 @@
 <!-- wp:column {"style":{"spacing":{"blockGap":"2px"}}} -->
 <div class="wp-block-column"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"20vw","className":"is-style-sticky-column-desktop"} -->
-<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading -->
-<h2 class="wp-block-heading">Adweek</h2>
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast","className":"is-style-offset","fontSize":"large"} -->
+<h2 class="wp-block-heading is-style-offset has-contrast-color has-text-color has-link-color has-large-font-size">Adweek</h2>
 <!-- /wp:heading -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"contrast","className":"is-style-extra-wide"} -->
+<hr class="wp-block-separator has-text-color has-contrast-color has-alpha-channel-opacity has-contrast-background-color has-background is-style-extra-wide" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator -->
 
 <!-- wp:paragraph -->
 <p>NYC</p>
@@ -268,9 +254,13 @@
 
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"20vw","className":"is-style-sticky-column-desktop"} -->
-<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading -->
-<h2 class="wp-block-heading">SKDK</h2>
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading {"className":"is-style-offset","fontSize":"large"} -->
+<h2 class="wp-block-heading is-style-offset has-large-font-size">SKDK</h2>
 <!-- /wp:heading -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"is-style-extra-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-extra-wide" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator -->
 
 <!-- wp:paragraph -->
 <p>NYC</p>
@@ -279,8 +269,8 @@
 
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:group {"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:heading -->
-<h2 class="wp-block-heading">Creative Developer</h2>
+<div class="wp-block-group"><!-- wp:heading {"fontSize":"medium"} -->
+<h2 class="wp-block-heading has-medium-font-size">Creative Developer</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
@@ -306,9 +296,13 @@
 
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"20vw","className":"is-style-sticky-column-desktop"} -->
-<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading -->
-<h2 class="wp-block-heading">w14 Labs</h2>
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading {"className":"is-style-offset","fontSize":"large"} -->
+<h2 class="wp-block-heading is-style-offset has-large-font-size">w14 Labs</h2>
 <!-- /wp:heading -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"is-style-extra-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-extra-wide" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator -->
 
 <!-- wp:paragraph -->
 <p>NYC</p>
@@ -330,9 +324,13 @@
 
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"20vw","className":"is-style-sticky-column-desktop"} -->
-<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading -->
-<h2 class="wp-block-heading">NYC Department of Sanitation</h2>
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading {"className":"is-style-offset","fontSize":"large"} -->
+<h2 class="wp-block-heading is-style-offset has-large-font-size">NYC Department of Sanitation</h2>
 <!-- /wp:heading -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"is-style-extra-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-extra-wide" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator -->
 
 <!-- wp:paragraph -->
 <p>NYC</p>
@@ -354,9 +352,13 @@
 
 <!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","width":"20vw","className":"is-style-sticky-column-desktop"} -->
-<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading -->
-<h2 class="wp-block-heading">Pratt Institute</h2>
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column-desktop" style="flex-basis:20vw"><!-- wp:heading {"className":"is-style-offset","fontSize":"large"} -->
+<h2 class="wp-block-heading is-style-offset has-large-font-size">Pratt Institute</h2>
 <!-- /wp:heading -->
+
+<!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"is-style-extra-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-extra-wide" style="margin-top:0;margin-bottom:0"/>
+<!-- /wp:separator -->
 
 <!-- wp:paragraph -->
 <p>NYC</p>
@@ -406,4 +408,4 @@
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
 
-<!-- wp:template-part {"slug":"footer-2"} /-->
+<!-- wp:template-part {"slug":"footer","area":"footer"} /-->
