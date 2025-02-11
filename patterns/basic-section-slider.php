64d197d63c8dacd6ba563c2b6041ab0482572515
hjude-design <harrison@hjude.com>
2025-02-11 07:47:46 -0500

    padding and rule to standard footer

---
+++ patterns/basic-section-slider.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Basic-Section-Slider
+ * Slug: bureaucrat/basic-section-slider
+ * Categories: 
+ */
+?>
+<!-- wp:columns {"verticalAlignment":null,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast"} -->
+<div class="wp-block-columns has-contrast-color has-text-color has-link-color"><!-- wp:column {"verticalAlignment":"top","width":"1.5rem","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"backgroundColor":"accent","textColor":"contrast","className":"is-style-sticky-column"} -->
+<div class="wp-block-column is-vertically-aligned-top is-style-sticky-column has-contrast-color has-accent-background-color has-text-color has-background has-link-color" style="flex-basis:1.5rem"><!-- wp:heading {"level":3,"style":{"typography":{"writingMode":"vertical-rl"},"elements":{"link":{"color":{"text":"var:preset|color|base-2"}}}},"textColor":"base-2"} -->
+<h3 class="wp-block-heading has-base-2-color has-text-color has-link-color" style="writing-mode:vertical-rl">Who</h3>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"100%"} -->
+<div class="wp-block-column" style="flex-basis:100%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
