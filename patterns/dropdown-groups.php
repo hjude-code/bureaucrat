f44baa319927332b1f198fa7d3be71f2a0febf4b
Harrison Jude <harrison@hjude.com>
2025-02-12 14:08:57 -0500

    added dropdown groups pattern for case studies

---
+++ patterns/dropdown-groups.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Title: Dropdown groups
+ * Slug: bureaucrat/dropdown-groups
+ * Categories: About
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Dropdowns"},"style":{"spacing":{"blockGap":"var:preset|spacing|150"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:details {"metadata":{"name":"teams"},"align":"wide","className":"is-style-section-Rule-H-Tp","style":{"spacing":{"blockGap":"var:preset|spacing|100"}},"fontSize":"large"} -->
+<details class="wp-block-details alignwide is-style-section-Rule-H-Tp has-large-font-size"><summary>the teams</summary><!-- wp:group {"layout":{"type":"grid","minimumColumnWidth":"20rem"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"is-style-section-outlined-L","style":{"spacing":{"blockGap":"var:preset|spacing|050"},"layout":{"columnSpan":1}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-section-outlined-L"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">Team</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Collaborated with closely with the marketing team from the initial inception of the new branding, to the production of assets for a multi channel marketing campaign, and in-person event branding.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-section-outlined-L","style":{"spacing":{"blockGap":"var:preset|spacing|050"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-section-outlined-L"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">Team</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Collaborated with closely with the marketing team from the initial inception of the new branding, to the production of assets for a multi channel marketing campaign, and in-person event branding.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></details>
+<!-- /wp:details -->
+
+<!-- wp:details {"metadata":{"name":"roles"},"align":"wide","className":"is-style-section-Rule-H-TpBtm","style":{"spacing":{"blockGap":"var:preset|spacing|100"}},"fontSize":"large"} -->
+<details class="wp-block-details alignwide is-style-section-Rule-H-TpBtm has-large-font-size"><summary>my role</summary><!-- wp:group {"layout":{"type":"grid","minimumColumnWidth":"20rem"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"is-style-section-outlined-L","style":{"spacing":{"blockGap":"var:preset|spacing|050"},"layout":{"columnSpan":1}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-section-outlined-L"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">role</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Collaborated with closely with the marketing team from the initial inception of the new branding, to the production of assets for a multi channel marketing campaign, and in-person event branding.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"is-style-section-outlined-L","style":{"spacing":{"blockGap":"var:preset|spacing|050"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-section-outlined-L"><!-- wp:heading {"level":5} -->
+<h5 class="wp-block-heading">role</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Collaborated with closely with the marketing team from the initial inception of the new branding, to the production of assets for a multi channel marketing campaign, and in-person event branding.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></details>
+<!-- /wp:details --></div>
+<!-- /wp:group -->
